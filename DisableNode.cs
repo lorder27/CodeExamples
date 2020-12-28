e2b05e8055abdcce24e20f60a8e16643bf57de65
EdiGregov <edi.gregov1@gmail.com>
2020-12-28 16:35:19 +0100

    GameExe

---
+++ DisableNode.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableNode : MonoBehaviour
+{
+    private bool setConnectionsBool = false;
+    public GameObject[] Nodes;
+
+
+
+
+    void OnTriggerEnter(Collider theCollision)
+    {
+        if(theCollision != this)
+        {
+            if (theCollision.tag == "Obstacle" || theCollision.tag == "Building")
+
+            {
+                gameObject.GetComponent<Node>().connections.Clear();
+                gameObject.GetComponent<Node>().enabled = false;
+                
+            }
+            if (theCollision.tag == "Node")
+            {
+                gameObject.GetComponent<Node>().connections.Add(theCollision.gameObject.GetComponent<Node>());
+            }
+        }
+
+        else
+        {
+            if (theCollision.tag == "Node")
+            {
+                theCollision.gameObject.SetActive(false);
+                Debug.Log("Disabled");
+            }
+        }
+        
+        
+    }
+
+    void OnTriggerExit(Collider theCollision)
+    {
+
+        if (theCollision.tag == "Obstacle" || theCollision.tag == "Building")
+
+        {
+            gameObject.GetComponent<Node>().enabled = true;
+            gameObject.GetComponent<Node>().connect = true;
+            gameObject.GetComponent<Node>().CoroutineStart();
+
+        }
+    }
+}
---
+++ SetMarkers.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetMarkers : MonoBehaviour
+{
+    public GameObject parent;
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Node") 
+        {
+            parent.GetComponent<Follower>().m_Start = other.gameObject.GetComponent<Node>();
+        }
+    }
+}
